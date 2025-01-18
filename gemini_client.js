5a77ace7fe262fab86f1b4b08c0e615aba3bf703
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2025-01-18 23:25:24 +0530

    Update gemini_client.js

--- gemini_client.js
+++ gemini_client.js
@@ -30,7 +30,7 @@ async function generate(prompt) {
   }
 }
 
-// uncomment for testing
+// uncomment this section for testing
 // async function main() {
 //   const prompt = "Introduce yourself in 20 words or less.";
 //   const result = await generate(prompt);
