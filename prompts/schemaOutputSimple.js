2caae76405d71d75ec9f52b5e793f66e7351d317
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2025-02-15 22:22:28 +0530

    Update schemaOutputSimple.js

--- prompts/schemaOutputSimple.js
+++ prompts/schemaOutputSimple.js
@@ -2,7 +2,7 @@ const generate = require("../gemini_client");
 
 async function main() {
   const prompt = `
-    List a few popular cookie recipes using this JSON schema:
+    List few popular cookie recipes using this JSON schema:
 
 Recipe = {'recipeName': string}
 Return: Array<Recipe>
